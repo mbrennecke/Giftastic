c137dc9906e59c8f67fa77186e0ac21b179f97e3
MBrennecke <MBrennecke10@gmail.com>
2018-08-10 17:43:42 -0600

    updates to javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 	
 	var topics = ["photography", "jewelry", "minecraft", "skyrim", "colors", "art", "star wars", "dungeons and dragons", "games", "fossils", "minerals", "dragons"];
 	var limit;
-	var rowInc=0;
-	var colCount=0;
+	var result;
 	
 	function renderButtons() {
 
@@ -20,29 +19,50 @@ $(document).ready(function() {
         }
       }
 	  
+	//creates matrix of gifs  
+	  
 	function generateMatrix(response) {
+		//clears any existing gifs, to start fresh
 		$("#gif-view").empty();
+		//loop for adding the gifs and creating the framework of the page
 		for (var i=0; i < limit; i++){
-			
+			//creates a bootstrap div class of row
 			var row = $("<div class='row'>");
-			
+			//appends the row to the parent element
 			$("#gif-view").append(row);
+				//Appends columns to the row that was created
+				//this loop will also modify i and becomes an internal itteration of the outer loop
 				for (var j=0; j<3; j++){
-					if (!response.data[i]){return;}
-					var col = $("<div class='col-sm-4'>");
-					var rating = response.data[i].rating;
+					//breaks out of the function if end of array is reached
+					if (!result[i]){return;}
+					//creates the bootstrap element with col size 4
+					var col = $("<div class='col-sm-4 col-marg'>");
+					//gets the value for rating of the gif
+					var rating = result[i].rating;
+					//creates the element for the rating
 					var ratingGif = $("<p>").text("Rating: "+rating);
-					var stillURL = response.data[i].images.fixed_width_still.url;
+					//gets the value for the still image
+					var stillURL = result[i].images.fixed_width_still.url;
+					//creates the image element for the gif
 					var stillGif = $("<img>").attr("src", stillURL);
+					stillGif.attr("data-still", stillURL);
+					stillGif.attr("data-state", "still");
+					//creates the image element for the gif
+					var movingGif = stillGif.attr("data-animated", result[i].images.fixed_width.url);
+					//appends the still gif and the rating of the gif to the column element
 					col.append(stillGif, ratingGif);
+					//adds the column to the row
 					$(row).append(col);
+					//manual itteration of i
 					i++;
-					console.log(i);
 				}
+				//manual decrementation of i or it skips every fourth number as i itterates due to end of loop
 				i--;
 		};
 	}
-	  
+	
+	// function that makes the ajax call to Giphy and then calls generateMatrix upon data receipt
+	
 	function displayGif() {
 		var gif = $(this).data("name");
 		var apiKey = "v3q7CXIe6uAx1Ua1TPclHLT9oZ6dJMct";
@@ -53,12 +73,12 @@ $(document).ready(function() {
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-			console.log(response);
+			result=response.data;
 			generateMatrix(response);
 		});
 	}
 
-      // Adding a click event listener to all elements with a class of "movie-btn"
+      // Adding a click event listener to all elements with a class of "gif-btn" and calls display function
       $(document).on("click", ".gif-btn", displayGif);
 	
 	renderButtons();
